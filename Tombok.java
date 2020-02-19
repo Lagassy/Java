1ec731a38ef21266b23209413dfc8bd6f449c4c5
Lagassy <56939352+Lagassy@users.noreply.github.com>
2020-02-19 14:28:41 +0100

    Update and rename Main.java to Tombok.java

--- Tombok.java
+++ Tombok.java
@@ -1,6 +1,6 @@
 package com.company;
 import java.util.Scanner;
-public class Main {
+public class Tombok {
 
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
